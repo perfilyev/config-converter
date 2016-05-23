97e6fb370f7142f91128a589a6365c9989b83d45
Igor Perfilyev <perfilyevhead@gmail.com>
2016-05-23 10:32:24 +0000

    #1. remove convert types switch + add type checker

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const fs = require('fs');
 const converter = require('./src/converter');
-const filetype = require('./lib/filetype');
+const extension = require('./lib/file-extension');
+const validator = require('./lib/format-validator');
+
+const type = (file) => validator(extension(file));
 
 const convert = (from, to) => {
-  const type = `${filetype(from)}->${filetype(to)}`;
-  const source = fs.readFileSync(from).toString();
+  const string = fs.readFileSync(from).toString();
   const out = (data) => {
     fs.writeFileSync(to, data);
     console.log('config save');
   };
 
-  converter(type)(source)(out);
+  converter(type(from))(type(to))(out)(string);
 };
 
 module.exports = convert;
---
+++ test/file-extension.js
@@ -0,0 +1,16 @@
+const extension = require('../lib/file-extension');
+const assert = require('chai').assert;
+
+describe('filtype', () => {
+  it('should be yml', () => {
+    assert.equal(extension('filename.yml'), 'yml');
+  });
+
+  it('should be yml, ignorcase', () => {
+    assert.equal(extension('filename.YmL'), 'yml');
+  });
+
+  it('should be blank', () => {
+    assert.equal(extension('filename'), '');
+  });
+});
