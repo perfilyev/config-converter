17859444e0b2ebc34e54bd4c95b9d7b12eee0359
Igor Perfilyev <perfilyevhead@gmail.com>
2016-05-28 14:45:06 +0000

    add abstraction layer to work with converter and codecs

---
+++ test/index.js
@@ -0,0 +1,41 @@
+const converter = require('../index');
+
+const convert = (source, destination, done) => converter.convert(source, destination).then(() => done(), err => console.log('error', err));
+
+describe('json to', () => {
+  const from = `${__dirname}/data/config.json`;
+
+  it('xml', done => {
+    const to = `${__dirname}/data/json.to.xml`;
+    convert(from, to, done);
+  });
+
+  it('yaml', done => {
+    const to = `${__dirname}/data/json.to.yml`;
+    convert(from, to, done);
+  });
+});
+
+describe('yaml to', () => {
+  const from = `${__dirname}/data/config.yml`;
+  it('xml', done => {
+    const to = `${__dirname}/data/yaml.to.xml`;
+    convert(from, to, done);
+  });
+  it('json', done => {
+    const to = `${__dirname}/data/yaml.to.json`;
+    convert(from, to, done);
+  });
+});
+
+describe('xml to', () => {
+  const from = `${__dirname}/data/config.xml`;
+  it('json', done => {
+    const to = `${__dirname}/data/xml.to.json`;
+    convert(from, to, done);
+  });
+  it('yaml', done => {
+    const to = `${__dirname}/data/xml.to.yml`;
+    convert(from, to, done);
+  });
+});
