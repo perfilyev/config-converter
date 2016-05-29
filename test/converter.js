45a66be9a658ad601eb0a826be948983bd4fe6db
Igor Perfilyev <perfilyevhead@gmail.com>
2016-05-29 09:54:34 +0000

    eslint es7 support + asserts error catch

--- test/converter.js
+++ test/converter.js
@@ -1,7 +1,7 @@
 import { assert } from 'chai';
 
 import { make as makeCodec } from '../src/codec';
-import { make as makeConverter, addCodec, hasCodec } from '../src/converter';
+import { make as makeConverter, addCodec, hasCodec, getCodec } from '../src/converter';
 import * as json from '../src/codecs/json';
 
 const jsonCodec = makeCodec('json', json.decode, json.encode);
@@ -9,16 +9,13 @@ const jsonCodec = makeCodec('json', json.decode, json.encode);
 describe('converter', () => {
   it('add codec', () => {
     const emptyConverter = makeConverter([]);
-    assert(!hasCodec(emptyConverter, 'json'));
+    assert(!hasCodec(emptyConverter, 'json'), 'should be empty');
     const jsonConverter = addCodec(emptyConverter, jsonCodec);
-    assert(hasCodec(jsonConverter, 'json'));
+    assert(hasCodec(jsonConverter, 'json'), 'has codec');
   });
-  
+
   it('unsupported codec', () => {
     const emptyConverter = makeConverter([]);
-    assert(!hasCodec(emptyConverter, 'json'));
+    assert.throws(() => getCodec(emptyConverter, 'json'), 'Unsupported codec');
   });
-  
-  
-  
 });
